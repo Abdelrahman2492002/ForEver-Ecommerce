03f90486a5cd98f7f30753b3556bd1de004ac665
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-11 22:32:22 +0200

    edit grid template columns

--- src/components/home/BestSeller.jsx
+++ src/components/home/BestSeller.jsx
@@ -14,8 +14,9 @@ const BestSeller = () => {
         industry. Lorem Ipsum has been the."
       />
       <div className="grid grid-cols-2 gap-x-7 gap-y-8 pt-11 sm:grid-cols-3 md:grid-cols-4 md:gap-y-11 lg:grid-cols-5">
-        {allProducts.slice(10, 15).map((product) => (
+        {allProducts.slice(10, 15).map((product, index) => (
           <ProductCard
+            key={index}
             image={product.image[0]}
             name={product.name}
             price={product.price}
