7f0929bf29d7b41c72f2dfa55d5a3fddd07c4683
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-11 22:25:58 +0200

    finish LatestCollection section

--- src/components/home/LatestCollection.jsx
+++ src/components/home/LatestCollection.jsx
@@ -17,15 +17,14 @@ const LatestCollection = () => {
     fetchProduct();
   }, []);
 
-  console.log(allProducts);
-
   return (
     <div className="mx-10 pt-5 pb-20 sm:mx-24 lg:mx-44">
       <SectionTitle text1="Latest" text2="Collections" />
       <SectionParagraph text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the." />
       <div className="grid grid-cols-2 gap-x-7 gap-y-8 pt-11 sm:grid-cols-3 md:grid-cols-4 md:gap-y-11 lg:grid-cols-5">
-        {allProducts.slice(0, 10).map((product) => (
+        {allProducts.slice(0, 10).map((product, index) => (
           <ProductCard
+            key={index}
             image={product.image[0]}
             name={product.name}
             price={product.price}
