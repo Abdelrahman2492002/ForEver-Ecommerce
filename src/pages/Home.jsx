9fe7a0225e868b18721dc665d7d803ca92229c39
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-12 11:57:41 +0200

    Add NewiLetterBoxs section to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BestSeller from "../components/home/BestSeller";
 import Hero from "../components/home/Hero";
 import LatestCollection from "../components/home/LatestCollection";
+import NewsLetterBox from "../components/home/NewsLetterBox";
 import OurPolicy from "../components/home/OurPolicy";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
       <LatestCollection />
       <BestSeller />
       <OurPolicy />
+      <NewsLetterBox />
     </div>
   );
 };
