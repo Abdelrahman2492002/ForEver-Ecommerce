118c7121ca94028399b8b3e75be7da8988ba2a57
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-12 11:58:44 +0200

    small edit

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import Logo from "./Logo";
+import Logo from "../common/Logo";
 import NavbarActions from "./actions/NavbarActions";
 import NavigationLinks from "./links/big-screen/NavigationLinks";
 
