46ae728be8927ff15b21f8769705ff3a41c4e75d
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-18 19:31:32 +0200

    add logic to Cart

--- src/components/navBar/actions/Cart.jsx
+++ src/components/navBar/actions/Cart.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { assets } from "../../../assets/assets";
+import { useShopStore } from "../../../store/shopStore";
 
 const Cart = () => {
+  const cartCount = useShopStore((state) => state.cartItemsCount());
+
   return (
     <div className="relative cursor-pointer">
       <img src={assets.cart_icon} alt="cart icon" className="w-5" />
       <span className="text-light absolute right-[-5px] bottom-[-8px] flex h-4 w-4 items-center justify-center rounded-full bg-black text-[11px]">
-        1
+        {cartCount}
       </span>
     </div>
   );
