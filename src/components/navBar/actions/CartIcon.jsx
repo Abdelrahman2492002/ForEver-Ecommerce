28670ff6cec1a6dfb81d309a5d4cc48c7d39685c
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-19 05:42:00 +0200

    Make the icon redirect to the Cart page

--- src/components/navBar/actions/CartIcon.jsx
+++ src/components/navBar/actions/CartIcon.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { assets } from "../../../assets/assets";
 import { useShopStore } from "../../../store/shopStore";
+import { Link } from "react-router";
 
 const CartIcon = () => {
   const cartCount = useShopStore((state) => state.cartItemsCount());
 
   return (
-    <div className="relative cursor-pointer">
+    <Link to="/cart" className="relative cursor-pointer">
       <img src={assets.cart_icon} alt="cart icon" className="w-5" />
       <span className="text-light absolute right-[-5px] bottom-[-8px] flex h-4 w-4 items-center justify-center rounded-full bg-black text-[11px]">
         {cartCount}
       </span>
-    </div>
+    </Link>
   );
 };
 
