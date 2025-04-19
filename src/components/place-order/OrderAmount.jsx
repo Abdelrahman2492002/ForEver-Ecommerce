0eb17405727e7d43d9054e3a5c3144b0dd67305c
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-19 19:06:43 +0200

    Make OrderAmount section

---
+++ src/components/place-order/OrderAmount.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useShopStore } from "../../store/shopStore";
+import { countCartAmount } from "../../store/utility";
+import SectionTitle from "../common/SectionTitle";
+import PriceRow from "../cart/PriceRow";
+
+const OrderAmount = () => {
+  const delivery__fee = useShopStore((state) => state.delivery__fee);
+  const cartItems = useShopStore((state) => state.cartItems);
+  const products = useShopStore((state) => state.products);
+  const subTotal = countCartAmount(cartItems, products);
+  return (
+    <div className="w-full">
+      <div className="flex justify-start text-lg sm:text-2xl">
+        <SectionTitle text1="cart" text2="total" />
+      </div>
+      <div className="flex flex-col gap-2 py-4 text-sm text-black">
+        <PriceRow title="Subtotal" value={subTotal} />
+        <PriceRow title="Shipping Free" value={delivery__fee} />
+        <PriceRow
+          title="total"
+          value={subTotal + delivery__fee}
+          isTitleBold={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default OrderAmount;
