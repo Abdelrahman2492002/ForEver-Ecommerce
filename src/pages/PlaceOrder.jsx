108b502f6fc9b244dd6adac77623d003854475e1
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-19 19:47:16 +0200

    add PrimaryButton to redirectTo my-order page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,17 +1,21 @@
 import DeliveryInfo from "../components/place-order/DeliveryInfo";
 import OrderAmount from "../components/place-order/OrderAmount";
 import PaymentMethod from "../components/place-order/PaymentMethod";
+import PrimaryButton from "../components/common/PrimaryButton";
 
 const PlaceOrder = () => {
   return (
     <div className="border-borderColor4 mx-7 mb-28 border-t py-10 sm:mx-16 md:py-16 lg:mx-32">
-      <div className="flex flex-col justify-between gap-x-4 gap-y-20 md:flex-row lg:items-center lg:gap-28">
+      <form className="flex flex-col justify-between gap-x-4 gap-y-20 md:flex-row lg:items-center lg:gap-28">
         <DeliveryInfo />
         <div className="min-w-80">
           <OrderAmount />
           <PaymentMethod />
+          <div className="float-right">
+            <PrimaryButton redirectTo="/my-order" text="Place Order" />
+          </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
