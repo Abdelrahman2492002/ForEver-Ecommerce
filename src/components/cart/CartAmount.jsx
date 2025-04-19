fbfc13575fbf1261d520e4a7fd7040b52ed33dde
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-19 18:57:38 +0200

    edit width

--- src/components/cart/CartAmount.jsx
+++ src/components/cart/CartAmount.jsx
@@ -11,21 +11,19 @@ const CartAmount = () => {
   const subTotal = countCartAmount(cartItems, products);
 
   return (
-    <div className="mx-7 mt-10 flex justify-end sm:mx-16 sm:mt-20 lg:mx-32">
-      <div className="w-full sm:w-2/4">
-        <div className="flex justify-start">
-          <SectionTitle text1="cart" text2="total" />
-        </div>
-        <div className="flex flex-col gap-2 py-4 text-sm text-black">
-          <PriceRow title="Subtotal" value={subTotal} />
-          <PriceRow title="Shipping Free" value={delivery__fee} />
-          <PriceRow
-            title="total"
-            value={subTotal + delivery__fee}
-            isTitleBold={true}
-          />
-          <CartCheckoutButton />
-        </div>
+    <div className="w-full">
+      <div className="flex justify-start text-lg sm:text-2xl">
+        <SectionTitle text1="cart" text2="total" />
+      </div>
+      <div className="flex flex-col gap-2 py-4 text-sm text-black">
+        <PriceRow title="Subtotal" value={subTotal} />
+        <PriceRow title="Shipping Free" value={delivery__fee} />
+        <PriceRow
+          title="total"
+          value={subTotal + delivery__fee}
+          isTitleBold={true}
+        />
+        <CartCheckoutButton />
       </div>
     </div>
   );
