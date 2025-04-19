434843a4fb53961ddffbe2126e7ad5452ebda063
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-19 06:48:16 +0200

    make CartCheckoutButton component

---
+++ src/components/cart/CartCheckoutButton.jsx
@@ -0,0 +1,9 @@
+const CartCheckoutButton = () => {
+  return (
+    <button className="text-light active:bg-activeBorder active:text-dark mt-4 w-full cursor-pointer self-end bg-black px-5 py-3 text-sm font-semibold uppercase transition sm:w-fit">
+      Proceed to checkout
+    </button>
+  );
+};
+
+export default CartCheckoutButton;
